d64ab2f279a6372c0c1a7fc69fe683d0a19dcafd
Carlos de Jesus Gutierrez Loaeza <7153253-GutierrezLoaezaCarlosdeJesus@users.noreply.gitlab.com>
2021-01-28 17:00:00 -0600

    ....

--- src/java/mbEstado/MbEstado.java
+++ src/java/mbEstado/MbEstado.java
@@ -30,11 +30,11 @@ public class MbEstado implements Serializable {
      * Creates a new instance of MbEstado
      */
     private ArrayList<SelectItem> lst = null;
-    private ArrayList<Estado> lstEstado ;
-     //private ArrayList<Estado> lstEstadoPais ;
+    private ArrayList<Estado> lstEstado;
+    //private ArrayList<Estado> lstEstadoPais ;
     private Estado seleccion;
     private Estado estadoNuevo;
-    
+
     public MbEstado() {
         estadoNuevo = new Estado();
         seleccion = new Estado();
@@ -50,33 +50,35 @@ public class MbEstado implements Serializable {
 
     public ArrayList<Estado> getLstEstado() {
         DAOEstado dao = new DAOEstado();
-        if(lstEstado == null){
+        if (lstEstado == null) {
             try {
                 lstEstado = new ArrayList<>();
-                for(Estado estado: dao.dameEstado()){
+                for (Estado estado : dao.dameEstado()) {
                     lstEstado.add(estado);
                 }
-            } catch (SQLException ex){
-                Logger.getLogger(MbEstado.class.getName()).log(Level.SEVERE,null,ex);
+            } catch (SQLException ex) {
+                Logger.getLogger(MbEstado.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
         return lstEstado;
     }
-public ArrayList<Estado> getLstEstadoPais() {
+
+    public ArrayList<Estado> getLstEstadoPais() {
         DAOEstado dao = new DAOEstado();
-        if (lstEstado == null){
+        if (lstEstado == null) {
             try {
                 lstEstado = new ArrayList<>();
-                
-                for (Estado estado : dao.dameEstadoPais2()){
+
+                for (Estado estado : dao.dameEstadoPais2()) {
                     lstEstado.add(estado);
                 }
-            } catch(SQLException ex){
-                Logger.getLogger(MbEstado.class.getName()).log(Level.SEVERE, null,ex);
+            } catch (SQLException ex) {
+                Logger.getLogger(MbEstado.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
         return lstEstado;
     }
+
     public void setLstEstado(ArrayList<Estado> lstEstado) {
         this.lstEstado = lstEstado;
     }
@@ -96,53 +98,53 @@ public ArrayList<Estado> getLstEstadoPais() {
     public void setEstadoNuevo(Estado estadoNuevo) {
         this.estadoNuevo = estadoNuevo;
     }
-    
-    public void cargarEstado(int idpais){
+
+    public void cargarEstado(int idpais) {
         DAOEstado dao = new DAOEstado();
         lst = new ArrayList<>();
         try {
-            
-            for(Estado estado: dao.dameEstado(idpais)){
-            SelectItem s = new SelectItem(estado.getIdestado(),estado.getEstado());
-            System.out.println("estado:" + estado.getEstado());
-            lst.add(s);
-        }
+
+            for (Estado estado : dao.dameEstado(idpais)) {
+                SelectItem s = new SelectItem(estado.getIdestado(), estado.getEstado());
+                System.out.println("estado:" + estado.getEstado());
+                lst.add(s);
+            }
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
-            Logger.getLogger(MbEstado.class.getName()).log(Level.SEVERE,null,ex);
+            Logger.getLogger(MbEstado.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    public void imprimirEstado(){
+
+    public void imprimirEstado() {
         estadoNuevo = seleccion;
         System.out.println(seleccion.getIdpais());
         System.out.println(seleccion.getIdestado());
         System.out.println(seleccion.getEstado());
     }
-    
-    public void limpiarEstado(){
-    lstEstado = null;
-    estadoNuevo = new Estado();
-    seleccion = null;
+
+    public void limpiarEstado() {
+        lstEstado = null;
+        estadoNuevo = new Estado();
+        seleccion = null;
     }
-    
-    public void guardarEstado(){
+
+    public void guardarEstado() {
         DAOEstado dao = new DAOEstado();
-        if(seleccion == null){
+        if (seleccion == null) {
             try {
                 dao.guardarEstado(estadoNuevo);
-                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,"Exito","Nuevo estado agregado exitosamente"));
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Exito", "Nuevo estado agregado exitosamente"));
             } catch (SQLException ex) {
                 System.out.println("Error al insertar estado");
-                Logger.getLogger(MbEstado.class.getName()).log(Level.SEVERE,null,ex);
+                Logger.getLogger(MbEstado.class.getName()).log(Level.SEVERE, null, ex);
             }
-        } else{
+        } else {
             try {
                 dao.actualizarEstado(estadoNuevo);
-                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,"Exito","Estado actualizado exitosamente"));
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Exito", "Estado actualizado exitosamente"));
             } catch (SQLException ex) {
                 System.out.println("Error al actualizar estado");
-                Logger.getLogger(MbEstado.class.getName()).log(Level.SEVERE,null,ex);
+                Logger.getLogger(MbEstado.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
         lstEstado = null;
